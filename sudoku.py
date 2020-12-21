80bd58b3c235fafdebe9d6e913d379ff4894f461
PiyushG14 <46423477+PiyushG14@users.noreply.github.com>
2020-12-22 01:48:00 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,4 @@
-#Setting up pygame
+#get a grid
 import pygame
 
 
@@ -9,6 +9,20 @@ def main():
     pygame.init()
     win = pygame.display.set_mode((WIDTH, WIDTH))
     pygame.display.set_caption("Sudoku")
+    win.fill(background_color)
+    
+    for i in range(0,10):
+        #Draw horizontal and vertical lines
+        if(i % 3 == 0):
+            #parameters: window, color, (starting x, starting y), (ending x, ending y), width)
+            pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), 4)
+            pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 4)
+            continue            
+        pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), 2)
+        pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2)
+        
+    pygame.display.update()
+    
     
     while True: 
         for event in pygame.event.get():
