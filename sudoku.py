f48661d8836238a1409791acb1d690d326803e85
PiyushG14 <46423477+PiyushG14@users.noreply.github.com>
2020-12-22 01:48:40 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,15 +1,24 @@
-#get a grid
+#api call and populating the grid
 import pygame
-
+import requests
 
 WIDTH = 550
 background_color = (251,247,245)
+orignal_grid_element_color = (52, 31, 151)
+
+
+response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
+grid = response.json()['board']
+grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
+
+
 
 def main():    
     pygame.init()
     win = pygame.display.set_mode((WIDTH, WIDTH))
     pygame.display.set_caption("Sudoku")
     win.fill(background_color)
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
     
     for i in range(0,10):
         #Draw horizontal and vertical lines
@@ -21,6 +30,12 @@ def main():
         pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i, 500), 2)
         pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2)
         
+    for i in range(0, len(grid[0])):
+        for j in range(0, len(grid[0])):
+            if(0 < grid[i][j] < 10 ):
+                value = myfont.render(str(grid[i][j]), True, orignal_grid_element_color) 
+                win.blit(value, ((j+1)*50 + 15 ,(i+1)*50 )) 
+            
     pygame.display.update()
     
     
